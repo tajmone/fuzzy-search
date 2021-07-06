8d21be1d932fe871f6ba63999e4879b36f938403
forever-new <86521150+forever-new@users.noreply.github.com>
2021-07-07 00:02:13 +1000

    Delphi bug fix 0.2.0 v2
    1. Added check for non-uppercaseable characters. e.g. numbers - so 'a4' doesn't get camel case bonus.
    2, Removed extra delphi index "pascal_index = 1" - it broke the score calculation in couple of places.
    3. Removed update for double byte chars, it was causing wrong calculation of unmatched count and other issues.
    4. Added initialization for internal static array so the output Matches array is not populated with memory garbage.
    5. Set option "first_letter_count" to 1 to align with other ports.
    6. Added test algorithm.

--- fts_fuzzy_match/0.2.0/delphi/uFuzzyMatching.pas
+++ fts_fuzzy_match/0.2.0/delphi/uFuzzyMatching.pas
@@ -10,11 +10,10 @@ unit uFuzzyMatching;
 // Original source: https://github.com/forrestthewoods/lib_fts/blob/master/code/fts_fuzzy_match.h
 // Blog: https://www.forrestthewoods.com/blog/reverse_engineering_sublime_texts_fuzzy_match/
 
-// Adapted slightly for own use, especially:
-// - Calculation corrected for double byte chars
-// - Added a match index offset of 1 to match Delphi/Pascal string indexes
-
 interface
+
+uses
+  System.SysUtils, System.Character;
 
 type
   TMatch = Byte;
@@ -30,19 +29,18 @@ implementation
 function FuzzyMatchRecursive(
   Pattern: PChar; Str: PChar; out OutScore: Integer;
   const StrBegin: PChar; const SrcMatches: PMatches; const Matches: PMatches; const MaxMatches: Integer; NextMatch: Integer;
-  RecursionCount: Integer; const RecursionLimit: Integer): Boolean;
+  RecursionCount: Integer; const RecursionLimit: Integer; MatchId: integer): Boolean;
 
 const
   sequential_bonus: Integer = 15;            // bonus for adjacent matches
   separator_bonus: Integer = 30;             // bonus if match occurs after a separator
   camel_bonus: Integer = 30;                 // bonus if match is uppercase and prev is lower
   first_letter_bonus: Integer = 15;          // bonus if the first letter is matched
-  first_letter_count: Integer = 2;           // How many letters count as 'first'. Set to 2, to skip the first, single letter prefix
+  first_letter_count: Integer = 1;           // How many letters count as 'first'. Try setting to 2 to skip the first, single letter prefix
 
   leading_letter_penalty: Integer = -5;      // penalty applied for every letter in str before the first match
   max_leading_letter_penalty: Integer = -15; // maximum penalty for leading letters
   unmatched_letter_penalty: Integer = -1;    // penalty for every letter that doesn't match
-  pascal_index = 1;                          // effectively a number to add to the match. Set to 1 to reflect delphi string indexes
 var
   RecursiveMatch: Boolean;
   BestRecursiveMatches: TMatches;
@@ -58,8 +56,10 @@ var
   prevIdx: Integer;
   Neighbor: Char;
   Curr: Char;
+
 begin
-  OutScore := 0;
+  OutScore := 100;
+  RecursiveMatches:= Default(TMatches);
 
   Inc(RecursionCount);
   if RecursionCount >= RecursionLimit then
@@ -75,7 +75,7 @@ begin
 
   while (Pattern^ <> #0) and (Str^ <> #0) do
   begin
-    if UpCase(Pattern^) = UpCase(Str^) then
+    if Pattern^ = Str^ then
     begin
       if NextMatch >= MaxMatches then
         Exit(False);
@@ -86,7 +86,7 @@ begin
         FirstMatch := False;
       end;
 
-      if FuzzyMatchRecursive(Pattern, Str+1, RecursiveScore, StrBegin, Matches, @RecursiveMatches[0], MaxMatches, NextMatch, RecursionCount, RecursionLimit) then
+      if FuzzyMatchRecursive(Pattern, Str+1, RecursiveScore, StrBegin, Matches, @RecursiveMatches[0], MaxMatches, NextMatch, RecursionCount, RecursionLimit, MatchId+1) then
       begin
         if (not RecursiveMatch) or (RecursiveScore > BestRecursiveScore) then
         begin
@@ -96,19 +96,18 @@ begin
         RecursiveMatch := True;
       end;
 
-      Matches[NextMatch] := Byte((Integer(Str) - Integer(StrBegin)) div SizeOf(Char)) + pascal_index;
+      Matches[NextMatch] := MatchId;
       Inc(NextMatch);
       Inc(Pattern);
     end;
     Inc(Str);
+    Inc(MatchId);
   end;
 
   Matched := Pattern^ = #0;
 
   if Matched then
   begin
-    while Str^ <> #0 do
-      Inc(Str);
 
     Penalty := leading_letter_penalty * matches[0];
     if Penalty < max_leading_letter_penalty then
@@ -116,7 +115,7 @@ begin
 
     Inc(OutScore, Penalty);
 
-    Unmatched := Integer(Str - StrBegin) - NextMatch;
+    Unmatched := Length(StrBegin) - NextMatch;
     Inc(OutScore, unmatched_letter_penalty * unmatched);
 
     for i := 0 to NextMatch - 1 do
@@ -136,10 +135,13 @@ begin
       begin
         Neighbor := StrBegin[currIdx - 1];
         Curr := StrBegin[Curridx];
-        if (NeighBor <> UpCase(Neighbor)) and (Curr = UpCase(Curr)) then
+        if NeighBor.IsLetter and Curr.IsLetter and
+           (NeighBor <> Neighbor.ToUpper)      and
+           (Curr      = Curr.ToUpper)
+        then
           Inc(OutScore, camel_bonus);
 
-        if (Neighbor = '.' ) or (Neighbor = '_') or (Neighbor = ' ') then
+        if Neighbor in ['.','_',' ','\'] then
           Inc(OutScore, separator_bonus);
       end;
 
@@ -174,10 +176,14 @@ end;
 function FuzzyMatch(const Pattern: String; const Str: String; out Score: Integer; var Matches: TMatches): Boolean;
 var
   RecursionCount, RecursionLimit: Integer;
+  PatternUC, StrUC: string;
 begin
   RecursionCount := 0;
   RecursionLimit := 10;
-  Result := FuzzyMatchRecursive(PChar(Pattern), PChar(Str), Score, PChar(Str), nil, @Matches[0], Length(Matches), 0, recursionCount, recursionLimit);
+  PatternUC := UpperCase(Pattern);
+  StrUC     := UpperCase(Str);
+
+  Result := FuzzyMatchRecursive(PChar(PatternUC), PChar(StrUC), Score, PChar(Str), nil, @Matches[0], Length(Matches), 0, recursionCount, recursionLimit, 0);
 end;
 
 end.
